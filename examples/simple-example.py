d16ce1342dce3f16d1b9f68876ec1c832539f34a
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2023-01-20 09:47:39 +0100

    add multi_rbf model

--- examples/simple-example.py
+++ examples/simple-example.py
@@ -7,11 +7,23 @@ def main():
     x = data['data']
     y = data['target']
 
-    model = OptimizedLSSVR(verbose=1)
-    model.fit(x, y)
+    model0 = OptimizedLSSVR(
+        verbose=1,
+    )
+    model0.fit(x, y)
+    p = model0.params_
+    print('final relative MSE:', model0.relative_mse_)
+    print('optimized parameters:', p)
 
-    print('final relative MSE:', model.relative_mse_)
-    print('optimized parameters:', model.params_)
+    model1 = OptimizedLSSVR(
+        kernel='multi_rbf',
+        lmbda0=p['lmbda'],
+        gamma0=p['gamma'],
+        verbose=1,
+    )
+    model1.fit(x, y)
+    print('final relative MSE:', model1.relative_mse_)
+    print('optimized parameters:', model1.params_)
 
 
 if __name__ == '__main__':
